33aa41e2044cbd6c557be6ebd76328f92489dfd9
Jan Peter <slajaa09@htlkaindorf.at>
2015-03-10 10:09:51 +0100

    Test assemble export

--- toyasm.js
+++ toyasm.js
@@ -1,12 +1,7 @@
 module.exports = {
   assemble: function (file) {
-    if(file && file.endsWith('.asm'))
-    {
-      if(file.indexOf('.//') == 0)
-        file = file.substr(3, file.length - 3);
-
-      var toy = require('./build/Release/toyasm');
-      toy.create(file);
-    }
+    alert(file);
+    //var toy = require('./build/Release/toyasm');
+    //toy.create(file);
   }
 };
